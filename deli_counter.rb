bdc5a1ff0c247fcf55dbb77ad8fdf9f3406a4458
Jesus Manny Baez <jesusmbaez@gmail.com>
2018-11-19 21:32:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  katz_deli.length == 0 ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{katz_deli.shift}.") 
+  
+  
+  if katz_deli.length == 0  
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
